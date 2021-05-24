cc554f051bd79f4c8f5898b1def6d830dd50c9d7
turnerdylan <dytu4626@colorado.edu>
2021-05-24 12:26:20 -0600

    fix

---
+++ Menu Scene/Assets/LevelManagement/Scripts/MenuManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelManagement
+{
+    public class MenuManager : MonoBehaviour
+    {
+        public Menu mainMenuPrefab;
+        public Menu settingsMenuPrefab;
+        public Menu creditsScreenPrefab;
+
+        [SerializeField]
+        private Transform _menuParent;
+
+        private Stack<Menu> _menuStack = new Stack<Menu>();
+
+        private void Awake()
+        {
+            InitializeMenus();
+        }
+
+        private void InitializeMenus()
+        {
+            if (_menuParent == null)
+            {
+                GameObject menuParentObject = new GameObject("Menus");
+                _menuParent = menuParentObject.transform;
+            }
+
+            Menu[] menuPrefabs = { mainMenuPrefab, settingsMenuPrefab, creditsScreenPrefab };
+
+            foreach (Menu prefab in menuPrefabs)
+            {
+                if (prefab != null)
+                {
+                    Menu menuInstance = Instantiate(prefab, _menuParent);
+                    if (prefab != mainMenuPrefab)
+                    {
+                        menuInstance.gameObject.SetActive(false);
+                    }
+                    else
+                    {
+                        OpenMenu(menuInstance);
+                    }
+                }
+            }
+        }
+
+        public void OpenMenu(Menu menuInstance)
+        {
+            if (menuInstance == null)
+            {
+                Debug.LogWarning("MENUMANAGER OpenMenu ERROR: invalid menu");
+                return;
+            }
+
+            if (_menuStack.Count > 0)
+            {
+                foreach (Menu menu in _menuStack)
+                {
+                    menu.gameObject.SetActive(false);
+                }
+            }
+
+            menuInstance.gameObject.SetActive(true);
+            _menuStack.Push(menuInstance);
+        }
+
+        public void CloseMenu()
+        {
+            if (_menuStack.Count == 0)
+            {
+                Debug.LogWarning("MENUMANAGER CloseMenu ERROR: No menus in stack!");
+                return;
+            }
+
+            Menu topMenu = _menuStack.Pop();
+            topMenu.gameObject.SetActive(false);
+
+            if (_menuStack.Count > 0)
+            {
+                Menu nextMenu = _menuStack.Peek();
+                nextMenu.gameObject.SetActive(true);
+            }
+        }
+    }
+}
